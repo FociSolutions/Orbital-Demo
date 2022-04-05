9264bd1c33eb104d1054bc7f0e3d5f9bb941ac4d
Emma Maddock <emma.maddock@focisolutions.com>
2022-04-05 15:52:22 -0400

    setting initial bash script structure along with comments and output statements for the user or developer

--- Scripts/PetStoreCurlTests.sh
+++ Scripts/PetStoreCurlTests.sh
@@ -1,40 +1,35 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2580
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red28\green31\blue34;\red246\green246\blue246;\red0\green0\blue109;
-\red210\green0\blue53;\red28\green31\blue34;\red246\green246\blue246;\red0\green0\blue109;\red210\green0\blue53;
-}
-{\*\expandedcolortbl;;\cssrgb\c14510\c16078\c18039;\cssrgb\c97255\c97255\c97255;\cssrgb\c0\c0\c50196;
-\cssrgb\c86667\c6667\c26667;\cssrgb\c14510\c16078\c18039;\cssrgb\c97255\c97255\c97255;\cssrgb\c0\c0\c50196;\cssrgb\c86667\c6667\c26667;
-}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+#!/bin/bash
 
-\f0\fs24 \cf0 #!/bin/bash\
-\
-PET_PATH=/pets\
-PET_ID_PATH=/pets/id\
-\
-#testing the POST action to /pets to add two pets\
-echo \'93POSTing a new pet and getting expected response\'94\
-\pard\pardeftab720\partightenfactor0
+#bash script that tests the demo endpoints of an app using the orbital mockdefinition service
+#and displaying responses in CLI as outlined in the pet-store-example.yml file
 
-\f1\fs21\fsmilli10800 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 curl \cf4 \strokec4 -v\cf2 \strokec2  \cf4 \strokec4 --header\cf2 \strokec2  \cf5 \strokec5 "Content-Type: application/json"\cf2 \strokec2  \cf4 \strokec4 -d\cf2 \strokec2  \cf5 \strokec5 "\{\\"value\\":\\"node JS\\"\}"\cf2 \strokec2  $API_PATH\
-\
-#testing the GET action to /pets to get all pets\
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+#FOLLOWING VARS ASSUME THAT THE HTTP PROTOCOL IS BEING HOSTED ON PORT 500
+#ALSO ASSUMING THAT THERE IS AN EXISTING PET WITH AN ID OF 1
+#AND NO EXISTING PET AT ID=200
+PET_PATH=http://localhost:5000/pets
+PET_ID_PATH=http://localhost:5000/pets/1
+PUT_PET_PATH=http://localhost:5000/pets/200
 
-\f0\fs24 \cf0 \cb1 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 echo \'93GETing a list of all pets and getting expected response of 200 OK\'94
-\f1\fs21\fsmilli10800 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 \
-\pard\pardeftab720\partightenfactor0
-\cf6 \cb7 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 curl \cf8 -v\cf6  \cf8 --header\cf6  \cf9 "Content-Type: application/json"\cf6  \cf8 -d\cf6  \cf9 "\{\\"value\\":\\"node JS\\"\}"\cf6  $API_PATH\
-\pard\pardeftab720\partightenfactor0
-\cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 \
-#testing the GET action to /pets/\{id\} to get the details of a specific pet\
-\
-#testing the PUT action to /pets/\{id\} to update details of an existing pet\
-\
-#testing the DELETE action to /pets/\{id\} to delete a specific pet}
+#testing the GET action to /pets to get all pets, should get a 200 OK response
+echo "GETting all pets from the /pets endpoint and expecting a 200 OK response"
+curl -v --header "Content-Type: application/json" -d "" $PET_PATH
+
+#testing the POST action to /pets, should get a 201 Created response
+echo "POSTing to the /pets endpoint and expecting a 201 Created response"
+curl -v -X POST --header "Content-Type: application/json" -d "" $PET_PATH
+
+#testing the GET action to /pets/{id} to get the details of a specific pet, should get a 200 OK response
+echo "GETting the pet with ID=1 from the /pets/1 endpoint and expecting a 200 OK"
+curl -v --header "Content-Type: application/json" -d "" $PET_ID_PATH
+
+#testing the PUT action to /pets/{id} to update details of an existing pet, should get a 200 OK
+echo "PUTting new detail updates to the pet with ID=1 at the /pets/1 endpoint and expecting a 200 OK"
+curl -v -X PUT --header "Content-Type: application/json" -d "" $PET_ID_PATH
+
+#testing the PUT action to /pets/{id} to create a new pet with specified ID, should get a 200 OK
+echo "PUTting a new pet with ID=200 at the /pets/200 endpoint and expecting a 200 OK"
+curl -v -X PUT --header "Content-Type: application/json" -d "" $PUT_PET_PATH
+
+#testing the DELETE action to /pets/{id} to delete a specific pet, should get a 200 OK
+echo "DELETE-ing the pet with ID=200 at the /pets/200 endpoint and expecting a 200 OK"
+curl -v -X DELETE --header "Accept: application/json" -d "" $PUT_PET_PATH

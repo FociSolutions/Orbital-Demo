9f009e6c541d14d32dfe8e8749983ae6d7f1f756
Emma Maddock <emma.madds@gmail.com>
2022-04-07 14:33:37 -0400

    added basic landing pages and initial search bar

---
+++ src/orbital-demo/src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'petstore-searchbar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.scss']
+})
+export class SearchBarComponent {
+  title = 'search-bar';
+}

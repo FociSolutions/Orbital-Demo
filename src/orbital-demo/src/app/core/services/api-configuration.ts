f336ca6aa216390cd0251c5dc58401c4f1bcdafa
Reuben Kogie <reuben.kogie@focisolutions.com>
2022-04-13 10:41:32 -0600

    RE: PR comments - Set rootUrl to HTTP and default Orbital port

--- src/orbital-demo/src/app/core/services/api-configuration.ts
+++ src/orbital-demo/src/app/core/services/api-configuration.ts
@@ -8,7 +8,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiConfiguration {
-  rootUrl: string = 'https://localhost:5001';
+  rootUrl: string = 'http://localhost:5000';
 }
 
 export interface ApiConfigurationInterface {
